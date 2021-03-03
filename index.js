a3c260efe5c76fbdec0b1ef94baadc87f08d9f2e
jonas lima <jlvieira248@gmail.com>
2021-03-03 16:34:37 -0300

    express

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ var express = require('express');
 var app = express();
 
 var htmlPath = path.join(__dirname, 'views');
-var cssPath = path.join(__dirname, 'bootstrap');
-var jsPath = path.join(__dirname, 'script');
+
 app.use(express.static(htmlPath));
-app.use(express.static(cssPath));
-app.use(express.static(jsPath));
+
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/PROTRATOR.html");
 });
